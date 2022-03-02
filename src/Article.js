00159c2bb20b514abc8a48829ba234a063161d8b
Joshua <yoonj200@gmail.com>
2022-03-02 14:07:04 -0600

    Completed assignment

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Article() {
-  return (
+function Article() { // declare function 'Article'
+  return ( // return value of JSX
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
+  ); // "when you want to put this component on the DOM, here's what it should be"
 }
 
 export default Article;
